42ff2666136b197b0b6a4b127c909ff7a008272c
mariobfernandes <N/A>
2023-10-17 15:31:08 +0100

    test review

--- tests/Engine.tests.ps1
+++ tests/Engine.tests.ps1
@@ -2,32 +2,27 @@
 Describe "Engine Config Load"{
     BeforeAll {    
         . $PSCommandPath.Replace('Engine.tests.ps1', '\..\Engine.ps1')    
-         $config=$PSScriptRoot + "\..\config\Applications.json"
-         $engine=[Engine]::new($config)
-         $engine.loadConfig()
+         $config=$PSScriptRoot + "\..\config\Applications.json"                  
     }
     
     It "Should have at application 930 loaded"{                     
+        $engine=[Engine]::new($config)  
         $engine.applications[0].id| Should  -Be 930
     }
     It "Should have a traffic indicator"{
-        $engine=[Engine]::new($config)
-        $engine.loadConfig()
+        $engine=[Engine]::new($config)        
         $engine.applications[0].indicators[0].name| Should  -Be "traffic"
     } 
     It "Should have a traffic indicator with more than 1 interval"{         
         $engine=[Engine]::new($config)
-        $engine.loadConfig()
         $engine.applications[0].indicators[0].intervals.count| Should  -BeGreaterThan 1
     } 
     It "Should have silent periods"{                 
         $engine=[Engine]::new($config)
-        $engine.loadConfig()
         $engine.applications[0].silent.count| Should  -BeGreaterThan 0
     } 
     It "Should have coorect levels set"{                 
         $engine=[Engine]::new($config)
-        $engine.loadConfig()         
         $engine.applications[0].levels.unavailable | Should  -Be 0
         $engine.applications[0].levels.critical | Should  -Be 0.3
         $engine.applications[0].levels.low | Should  -Be 0.5
